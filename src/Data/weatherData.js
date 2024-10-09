c2830065be7e936826970fa5e1a8ecc5f7725bba
Animatedguess <105106737+Animatedguess@users.noreply.github.com>
2024-10-10 01:46:43 +0530

    using environment variable

--- src/Data/weatherData.js
+++ src/Data/weatherData.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const weatherData = async (cityName) => {
   try {
     const res = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=95e03e4fc4b1ae9f6008755e87e588cf&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${process.env.REACT_APP_APP_ID}&units=metric`
     );
     return res.data;
   } catch (error) {
